90af6653db7cbb8d7632bdfc44f56b60f15ee058
Alliswell-soku <89200643+Alliswell-soku@users.noreply.github.com>
2021-09-11 18:07:13 +0900

    Updated

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const togglebtn = document.querySelector(".navbar_togglebtn");
 const menu = document.querySelector(".navbar_menu");
 const icons = document.querySelector(".navbar_icons");
@@ -7,13 +6,3 @@ togglebtn.addEventListener("click", () => {
   menu.classList.toggle("active");
   icons.classList.toggle("active");
 });
-=======
-const togglebtn = document.querySelector(".navbar_togglebtn");
-const menu = document.querySelector(".navbar_menu");
-const icons = document.querySelector(".navbar_icons");
-
-togglebtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  icons.classList.toggle("active");
-});
->>>>>>> 41c7b1976aa518f154b8bb4a4228256e9808c74f
